7b50fea70441347fb1d3bfea4516c4f3b4d54002
Elaine Thai <ethai1827@gmail.com>
2019-05-10 04:15:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
+  else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
 }
